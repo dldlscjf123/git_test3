a03d6e8ad43c8844c051af562f940235b78a1d5c
dldlscjf123 <dldlscjf1231@naver.com>
2025-04-02 11:05:18 +0900

    최초 커밋

---
+++ src/main/java/org/example/git_test3/Main.java
@@ -0,0 +1,13 @@
+package org.example.git_test3;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] a = { 4, 8, 2, 6, 10, 3, 1, 7, 9, 5 };
+        Sort.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
