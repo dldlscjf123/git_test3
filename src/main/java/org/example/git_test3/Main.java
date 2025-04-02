4e9a8232372c4c413ac1d4ebf6a04fd7db4d311f
dldlscjf123 <dldlscjf1231@naver.com>
2025-04-02 11:14:26 +0900

    랜덤 데이터 생성

--- src/main/java/org/example/git_test3/Main.java
+++ src/main/java/org/example/git_test3/Main.java
@@ -1,10 +1,19 @@
 package org.example.git_test3;
 
 import java.util.Arrays;
-
+import java.util.Random;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static int[] createArray(int size, int minValue, int maxValue) {
+        Random random = new Random();
+        int[] a = new int[size];
+        for (int i = 0; i < a.length; ++i) {
+            a[i] = random.nextInt(maxValue - minValue + 1) + minValue;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
         int[] a = { 4, 8, 2, 6, 10, 3, 1, 7, 9, 5 };
         Sort.sort(a);
